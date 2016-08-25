7d680b292d5482b080346ff77669f13b0dee8b5d
Luke Frisken <l.frisken@gmail.com>
2016-08-25 15:05:17 +1000

    setting up database migration

--- Assignment1/Version1/app/models.py
+++ Assignment1/Version1/app/models.py
@@ -1,16 +1,19 @@
 from app import app
 from flask_sqlalchemy import SQLAlchemy
+import datetime
 
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True)
-    email = db.Column(db.String(120), unique=True)
+class Input(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	person_name = db.Column(db.String(120))
+	date_of_birth = db.Column(db.Date)
+	timestamp = db.Column(db.DateTime)
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
+	def __init__(self, person_name, date_of_birth):
+		self.person_name = person_name
+		self.date_of_birth = date_of_birth
+		self.timestamp = datetime.datetime.utcnow()
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+	def __repr__(self):
+		return "<Input {0}>".format(self.id)
