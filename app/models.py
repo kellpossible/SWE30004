af1c591f9a506848cea911588b4d8087f138a8d9
Luke Frisken <l.frisken@gmail.com>
2016-10-21 04:20:30 -0400

    version 2 migration 1

--- app/models.py
+++ app/models.py
@@ -7,12 +7,14 @@ db = SQLAlchemy(app)
 
 class Input(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    person_name = db.Column(db.String(120))
     first_name = db.Column(db.String(120))
     last_name = db.Column(db.String(120))
     date_of_birth = db.Column(db.Date)
     timestamp = db.Column(db.DateTime)
 
     def __init__(self, first_name, last_name, date_of_birth):
+        self.person_name = None
         self.first_name = first_name
         self.last_name = last_name
         self.date_of_birth = date_of_birth
@@ -22,7 +24,10 @@ class Input(db.Model):
         return "<Input {0}>".format(self.id)
 
     def full_name(self):
-        return self.first_name + " " + self.last_name
+        if self.person_name is None:
+            return self.first_name + " " + self.last_name
+        else:
+            return self.person_name
 
     def calculate_earth_age_days(self):
         today = datetime.datetime.now()
